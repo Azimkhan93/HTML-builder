6af395caf9d1d1512c9e6b6ad5a17439cabe5726
Azimkhan93 <aabdulsatarov@gmail.com>
2023-05-07 23:10:19 +0500

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
 
+
+
+fsPromises.mkdir(path.resolve(__dirname, 'files-copy'), {recursive: true} )
+
+fs.readdir(path.resolve(__dirname, 'files'), function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  } 
+  //listing all files using forEach
+  files.forEach(function (file) {
+      fsPromises.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file))
+      console.log(file); 
+  });
+});
